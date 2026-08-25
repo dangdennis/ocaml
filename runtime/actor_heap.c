fd9dccb5728d16102dcab633e7b0c1596bb758e3
Dennis Dang <22418429+dangdennis@users.noreply.github.com>
2026-08-25 05:51:05 -0400

    actor-mvp: add arena copyright headers

--- runtime/actor_heap.c
+++ runtime/actor_heap.c
@@ -2,9 +2,13 @@
 /*                                                                        */
 /*                                 OCaml                                  */
 /*                                                                        */
-/*   This file is distributed under the terms of the GNU Lesser General   */
-/*   Public License version 2.1, with the special exception on linking     */
-/*   described in the file LICENSE.                                       */
+/*                             Dennis Dang                                */
+/*                                                                        */
+/*   Copyright 2026 Dennis Dang                                           */
+/*                                                                        */
+/*   All rights reserved.  This file is distributed under the terms of    */
+/*   the GNU Lesser General Public License version 2.1, with the          */
+/*   special exception on linking described in the file LICENSE.          */
 /*                                                                        */
 /**************************************************************************/
 
