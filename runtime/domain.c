56257dbdb9195c1488f0e22af33bfc38158fe217
Dennis Dang <22418429+dangdennis@users.noreply.github.com>
2026-08-25 05:53:34 -0400

    actor-mvp: add deterministic actor scheduling

--- runtime/domain.c
+++ runtime/domain.c
@@ -879,6 +879,7 @@ static void domain_create(uintnat initial_minor_heap_wsize,
 
   domain_self = d;
   caml_state = domain_state;
+  domain_state->actor_scheduler = NULL;
   domain_state->actor_heap = NULL;
 
   domain_state->young_limit = 0;
@@ -2353,6 +2354,7 @@ void caml_domain_terminate(bool last)
   }
 
   if (!last) caml_assert_shared_heap_is_empty(domain_state->shared_heap);
+  CAMLassert(domain_state->actor_scheduler == NULL);
   CAMLassert(domain_state->actor_heap == NULL);
 
   /* [domain_state] may be reused by a fresh domain here, now that we
